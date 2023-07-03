6af71949746425a789c09fb9ecd7cb3e9b39c2c1
lordyhas <dev.haspro@gmail.com>
2023-07-03 23:03:41 +0200

    version 0.1.2

--- jira-to-sparkapi_rmi_client/src/main/java/org/example/Main.java
+++ jira-to-sparkapi_rmi_client/src/main/java/org/example/Main.java
@@ -3,8 +3,8 @@ package org.example;
 
 import com.google.gson.Gson;
 
-import com.google.gson.JsonElement;
 import org.example.domain.Assignee;
+import org.example.domain.TaskEvolution;
 import org.example.jira.JiraConnection;
 
 import java.io.IOException;
@@ -12,53 +12,70 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
-import java.util.Collections;
+
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
-import java.util.Objects;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main {
+    static TaskEvolution[] taskData = new TaskEvolution[]{
+            new TaskEvolution(0, 1, 0, 0,0, 0, 0, 0, 0, new Date(), 0),
+            new TaskEvolution(0, 9, 0, 5,1, 0, 0, 0, 0, new Date(), 40),
+            new TaskEvolution(0, 7, 4, 5,1, 0, 0, 0, 0, new Date(), 16),
+            new TaskEvolution(0, 9, 0, 5,1, 0, 0, 0, 0, new Date(), 1)
+    };
+
+    static Assignee[] assigneeData = new Assignee[]{
+            new Assignee(0, "Acacia Nday", "5f6bc1db06e34200711db7ee"),
+            new Assignee(40, "Marinakinja", "712020:d758e715-289d-4667-8e1c-bbbc2c21ee9b"),
+            new Assignee(16, "Hassan Tsheleka", "712020:794d753c-e996-4e91-ac5c-775e7d8bf0e9"),
+            new Assignee(1, "Benjamin Oleko", "627cba6c6ba8640069cf1faa"),
+    };
     public static void main(String[] args) {
         System.out.println("=== === RMI Client === ===");
+        String sparkUrl = "http://localhost:4567/jira/services";
+
         try{
             String url = "rmi://localhost:5097/jira-api";
 
-            JiraConnection jiraConnection = (JiraConnection)  Naming.lookup(url);
+            //JiraConnection jiraConnection = (JiraConnection)  Naming.lookup(url);
 
 
-            try {
-                List<Assignee> results = jiraConnection.getAssignees();
-                System.out.println("Size of Assignees :" + results.size()+"\n");
-                System.out.println("Assignees : " + results.get(0)+" ...\n");
 
-                String json = new Gson().toJson(results.get(0));
-                Assignee result0 = new Gson().fromJson(json, Assignee.class);
+            //List<Assignee> results = jiraConnection.getAssignees();
+            //System.out.println("Size of Assignees :" + results.size()+"\n");
+            //System.out.println("Assignees : " + results.get(0)+" ...\n");
 
-                System.out.println("Gson operations...");
+            String json = new Gson().toJson(assigneeData[2]);
+            Assignee result0 = new Gson().fromJson(json, Assignee.class);
 
-                System.out.println("Assignees : " + result0+" ...\n");
-                //Http.post("", gson);
-                /*for(String str: results){
+            System.out.println("Gson operations...");
+
+            System.out.println("Assignees : " + result0+"\n");
+            System.out.println("sent to : " + sparkUrl+"/assignees...");
+
+            Http.post(sparkUrl+"/assignees", json);
+            /*for(String str: results){
                     Issue issue = new Gson().fromJson(str, Issue.class);
 
                     //String reporterAccountId = issue.getReporter().getAccountId();
                     String reporterAccountId = Objects.requireNonNull(issue.getReporter()).getAccountId();
                     System.out.print("Reporter: "+ jiraConnection.getUser(reporterAccountId));
                     System.out.println(", Key: "+issue.getKey() + ", Title: "+issue.getSummary() + ", Status: " + issue.getStatus().getName());
-                }*/
-                //System.out.println("Ben a "+results.size() +" open issues");
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+               }*/
+            //System.out.println("Ben a "+results.size() +" open issues");
+
 
         }catch(RemoteException | MalformedURLException e){
             System.out.println("Failed to open the server");
             System.out.println("Error Message : "+e);
-        } catch (NotBoundException ex) {
+        } /*catch (NotBoundException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }*/catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
--- jira-to-sparkapi_rmi_client/src/main/java/org/example/domain/Assignee.java
+++ jira-to-sparkapi_rmi_client/src/main/java/org/example/domain/Assignee.java
@@ -1,19 +1,39 @@
 package org.example.domain;
 
 import java.io.Serializable;
-
-public class Assignee implements Serializable {
+public class Assignee implements Serializable{
     private final int id;
     private String name;
-    private final String email;
+    private final String email = null;
     private String jiraAccountId;
 
-    public Assignee(int id, String name, String email, String jiraAccountId) {
+    public Assignee(int id, String name, String jiraAccountId) {
         super();
         this.id = id;
         this.name = name;
-        this.email = email;
+        //this.email = email;
         this.jiraAccountId = jiraAccountId;
+    }
+
+    @Override
+    public String toString() {
+        return "Assignee{id="+this.id+", name="+this.name+", jiraAccountId="+this.jiraAccountId+"}";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        Assignee other = (Assignee) obj;
+        return this.name.equals(other.name) &&
+                this.jiraAccountId.equals(other.jiraAccountId);
+    }
+
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + name.hashCode();
+        result = 31 * result + jiraAccountId.hashCode();
+        return result;
     }
 
     public int getId() {
