29256117853f73849f348bf6ec2d3e8e7359570c
lordyhas <dev.haspro@gmail.com>
2023-07-08 18:41:37 +0200

    version 0.1.3 | add Http on corba server

---
+++ corba_server/src/corbaserver/Http.java
@@ -0,0 +1,131 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package corbaserver;
+
+import java.io.IOException;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+
+
+/**
+ *
+ * @author lordyhas
+ */
+public class Http {
+    
+    
+    
+    public static String get(String url, int id) throws IOException {
+        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+
+            //HTTP GET method
+            HttpGet httpget = new HttpGet(url);
+            
+            System.out.println("Executing get request" + httpget.getRequestLine());
+
+            // Create a custom response handler
+            ResponseHandler<String> getResponseHandler = response -> {
+                int status = response.getStatusLine().getStatusCode();
+                if (status >= 200 && status < 300) {
+                    HttpEntity entity = response.getEntity();
+                    return entity != null ? EntityUtils.toString(entity) : null;
+                } else {
+                    throw new ClientProtocolException("Unexpected [get] response status: " + status);
+                }
+            };
+            String responseBody = httpclient.execute(httpget, getResponseHandler);
+            System.out.println("----------------------------------------");
+            System.out.println(responseBody);
+            return responseBody;
+        }
+        
+      
+    }
+    
+    public static String get(String url) throws IOException {
+        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+
+            //HTTP GET method
+            HttpGet httpget = new HttpGet(url);
+            
+            System.out.println("Executing get request" + httpget.getRequestLine());
+
+            // Create a custom response handler
+            ResponseHandler<String> getResponseHandler = response -> {
+                int status = response.getStatusLine().getStatusCode();
+                if (status >= 200 && status < 300) {
+                    HttpEntity entity = response.getEntity();
+                    return entity != null ? EntityUtils.toString(entity) : null;
+                } else {
+                    throw new ClientProtocolException("Unexpected [get] response status: " + status);
+                }
+            };
+            String responseBody = httpclient.execute(httpget, getResponseHandler);
+            System.out.println("----------------------------------------");
+            System.out.println(responseBody);
+            return responseBody;
+        }
+        
+      
+    }
+    
+    
+    
+    
+
+    public static void post(String url, String json) throws IOException {
+        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            HttpPost httpPost = new HttpPost(url);
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-type", "application/json");
+
+            StringEntity stringEntity = new StringEntity(json);
+            httpPost.setEntity(stringEntity);
+
+            System.out.println("Executing post request  " + httpPost.getRequestLine());
+
+            // Create a custom response handler
+            ResponseHandler<String> postResponseHandler = response -> {
+                int status = response.getStatusLine().getStatusCode();
+                if (status >= 200 && status < 300) {
+                    HttpEntity entity = response.getEntity();
+                    return entity != null ? EntityUtils.toString(entity) : null;
+                } else {
+                    throw new ClientProtocolException("Unexpected [post] response status: " + status);
+                }
+            };
+            String responseBody = httpclient.execute(httpPost, postResponseHandler);
+            System.out.println("----------------------------------------");
+            System.out.println(responseBody);
+        }
+    }
+
+    public static void put() throws IOException {
+
+        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            HttpPut httpPut = new HttpPut("http://localhost:8080/api/v1/users");
+            httpPut.setHeader("Accept", "application/json");
+            httpPut.setHeader("Content-type", "application/json");
+        }
+    }
+
+    public static void delete() throws IOException {
+        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            HttpDelete httpDelete = new HttpDelete("http://localhost:8080/api/v1/users");
+            httpDelete.setHeader("Accept", "application/json");
+            httpDelete.setHeader("Content-type", "application/json");
+        }
+    }
+}
