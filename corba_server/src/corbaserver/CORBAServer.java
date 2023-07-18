2706e48b6e7e816019363fad716cc1aeaeb92952
lordyhas <dev.haspro@gmail.com>
2023-07-18 13:25:37 +0200

    version 0.1.3+5

--- corba_server/src/corbaserver/CORBAServer.java
+++ corba_server/src/corbaserver/CORBAServer.java
@@ -8,8 +8,8 @@ package corbaserver;
 //import BankApp.OperationBancaireHelper;
 import GetData.RetrieveData;
 import GetData.RetrieveDataHelper;
-import JiraIssue.Assignee;
-import JiraIssue.TaskEvolution;
+//import JiraIssue.Assignee;
+//import JiraIssue.TaskEvolution;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -33,13 +33,7 @@ public class CORBAServer {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        
-        getDataTest();
-        //return;
         try {
-            
-    
             // initialisation du bus ORB
             ORB orb = ORB.init(args, null);
             // activation du POA
@@ -64,45 +58,12 @@ public class CORBAServer {
             orb.run();   
             
         }catch(Exception e){
-            System.out.println("Erreur lors de l ouverture du serveur");
+            System.out.println("Erreur lors de l'ouverture du serveur "+e);
         }
     }
     
     
-    static void getDataTest() {
-        String sparkUrl = "http://localhost:4567/jira/services";
-        //http://localhost:4567/jira/services/assignees
-        /// get all assignees from spark api
-        try{
-            String assigneesJson = Http.get(sparkUrl+"/assignees");
-
-            /// get all tasks from spark api
-            String tasksJson = Http.get(sparkUrl+"/tasks");
-            
-            System.out.println(assigneesJson);
-            
-            
-            JsonObject jsonObject = new Gson().fromJson(assigneesJson, JsonObject.class);
-
-            // Get the value of the key as a JsonElement
-            JsonElement dataElement = jsonObject.get("data");
-
-            // Convert the JsonElement to an array of User objects using Gson
-            //User[] users = gson.fromJson(dataElement, User[].class);
-
-            
-            /// convert assigneesJson to list of assignees
-            List<Assignee> assignees = new Gson().fromJson(dataElement, new TypeToken<List<Assignee>>(){}.getType());
-            //List<TaskEvolution> tasks = new Gson().fromJson(tasksJson, new TypeToken<List<TaskEvolution>>(){}.getType());
-
-            System.out.println("Here are assignee : ");
-            for(Assignee assignee : assignees){
-                System.out.println(assignee);
-            }
-        }catch (IOException e) {
-            System.out.println("Error : "+e);
-        }
-    }
+    
     
     
 }
