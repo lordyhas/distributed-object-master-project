8c4491c207edd04a9d3d8ef81bb82cee30c2509a
lordyhas <dev.haspro@gmail.com>
2023-07-25 12:05:45 +0200

    version 0.1.3+8 | corba - review

--- corba_client/src/corbaclient/CORBAClient.java
+++ corba_client/src/corbaclient/CORBAClient.java
@@ -38,8 +38,25 @@ public class CORBAClient {
             
             System.out.println("Here are assignee : ");
             for(Assignee assignee : assignees){
-                System.out.println("Assignee{"+assignee.id+","+assignee.name+","+assignee.jiraAccountId+"}");
-            }      
+                System.out.println("Assignee{id="+assignee.id+", name="+assignee.name+", jiraAccountId="+assignee.jiraAccountId+"}");
+            }
+            
+            System.out.println("Here are assignee : ");
+            for(TaskEvolution task : tasks){
+                System.out.println("TaskEvolution{" +
+                "id="+task.id+", " +
+                "openTaskCount="+task.openTaskCount+", " +
+                "blockedTaskCount="+task.blockedTaskCount+", " +
+                "backlogTaskCount="+task.backlogTaskCount+", " +
+                "inProgressTaskCount="+task.inProgressTaskCount+", " +
+                "inReviewTaskCount="+task.inReviewTaskCount+", " +
+                "doneTaskCount="+task.doneTaskCount+", " +
+                "oldTaskCount="+task.oldTaskCount+", " +
+                "newTaskCount="+task.newTaskCount+", " +
+                "statisticDate="+task.statisticDate+", " +
+                "assigneeId="+task.assigneeId+
+                "}");
+            }
         }catch(Exception e){
             System.out.println("Erreur lors de l'ouverture du serveur \nError : "+e);
         }
